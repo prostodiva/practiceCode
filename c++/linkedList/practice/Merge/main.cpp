47f3585f059c9a1405575e1babef8ca15c966114
prostodiva <rkattsyna@mail.ru>
2025-06-20 19:38:38 -0700

    Add merge sort

--- c++/linkedList/practice/Merge/main.cpp
+++ c++/linkedList/practice/Merge/main.cpp
@@ -1,12 +1,5 @@
 //Merge two lists. Don't need to create the third list. Time - O(m+n)
-//check the smallest first value in the lists(compare)
-//assign two pointers(temp and last) to the smallerst value
-// relink the head to next node
-// last->next = nullptr
-// else - the dame thing for the second list
-// compare again two lists, relink this node to the node from the second list
-// make ladt next to null
-// continue the steps
+//devide the code in four parts
 
 
 #include <iostream>
@@ -30,9 +23,60 @@ void PushAtFront(Node<T>*& head, T value) {
 }
 
 template<typename T>
-Node<T>* Merge(Node<T>*& firstHead, Node<T>*& secondHead) {
-
-}
+Node<T>* Merge(Node<T>*& first, Node<T>*& second) {
+  //handle if the first node if nullptr
+  if (first == nullptr) return second;
+  if (second == nullptr) return first;
+  //part 1:
+  //    - create two pointers
+  //    - compare the first two nodes from the lists. handle cases if value are equal
+  //    - move both pointers to the smallers node
+  //    - make the last node point to nullptr
+    Node<T>* temp = nullptr;
+    Node<T>* last = nullptr;
+      if(first->data <= second->data) {
+        temp = last = first;
+        first = first->next;
+        last->next = nullptr;
+      } else {
+        if(second->data <= first->data) {
+          temp = last = second;
+          second = second->next;
+          last->next = nullptr;
+        }
+      }
+      //Part 2:
+      //    - traverse until one of the list is not nullptr
+      //    - compare data
+      //    - move last->next on the smallest
+      //    - move last on the head
+      //    - move head forward
+      //    - make last nullptr
+      while (first != nullptr && second != nullptr) {
+        if (first->data <= second->data) {
+          last->next = first;
+          last = first;
+          first = first->next;
+          last->next = nullptr;
+        } else {
+          if (second->data <= first->data) {
+            last->next = second;
+            last = second;
+            second = second->next;
+            last->next = nullptr;
+          }
+        }
+        //Part 3:
+        //    - if there are remaining nodes - point to the node
+        //    - check for != nullptr
+        if (first != nullptr) {
+          last->next = first;
+        } else {
+          last->next = second;
+        }
+      }
+      return temp;
+  }
 
 
 template<typename T>
@@ -58,7 +102,9 @@ int main() {
     PushAtFront(secondList, 15);
     PrintList(secondList);           //15 25 35
 
-
+    Node<int>* thirdList = firstList;
+    Merge(firstList, secondList);
+    PrintList(thirdList);
 
     return 0;
 }
