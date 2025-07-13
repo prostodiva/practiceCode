6a9f839f5d0562b6bd6e029d895d6ce932dad8f2
prostodiva <rkattsyna@mail.ru>
2025-07-13 16:52:37 -0700

    Circular LL. Insert

--- c++/linkedList/practice/circular/main.cpp
+++ c++/linkedList/practice/circular/main.cpp
@@ -35,6 +35,39 @@ void PrintList(Node* head) {
   } while (temp != head);
 }
 
+//insert before head and after the last is the same procedure.
+//pseudocode:
+//insert before head(pos == 0):
+//  - create a node
+//if the list is empty:
+//   - make the first node point to itself
+//if not:
+//  - create temp, traverse it until temp->next != nullptr
+//  - make a newNode->next point to head, temp->next to newNode
+//insert at position the procedure is the same as in linear linked list
+void Insert(Node*& head, int position, int value) {
+  Node* newNode = new Node(value);
+  Node* temp = head;
+  if (position == 0) {
+      if (head == nullptr) {
+        newNode->next = newNode;
+        head = newNode;
+      }
+      while (temp->next != head) {
+        temp = temp->next;
+      }
+      newNode->next = head;
+      temp->next = newNode;
+      head = newNode;    //optional
+  } else {
+    for (int i = 0; i < position - 1; i++) {
+      temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+  }
+}
+
 void DeleteList(Node*& head) {
   if (head == nullptr) return;
 
@@ -55,6 +88,9 @@ int main() {
   CreateList(circularList, 20);
   CreateList(circularList, 30);
 
+  PrintList(circularList);
+  Insert(circularList, 0, 5);
+  Insert(circularList, 2, 15);
   PrintList(circularList);
   DeleteList(circularList);
   
